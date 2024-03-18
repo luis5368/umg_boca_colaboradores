daf87658320cca75aa1872aa85983439a3762e4a
luis5368 <111549171+luis5368@users.noreply.github.com>
2024-03-17 23:50:49 -0600

    Add files via upload

---
+++ parte1.cpp
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+// Ejemplo Clases en C++ con (setter and getter)
+
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<fstream>
+using namespace std;
+
+
+
+
+//clase persona
+class Persona{
+	//ofstream archivo_salida("file.txt");
+	private:   //Atributos
+	    string nombres;
+	    string apellidos;
+	    string direccion;
+	    int telefono;
+    	int dia, mes, anio;
+	    
+	    //aca tengo que colocar los nuevos datos
+	public: // metodos//setters
+	    Persona(string, string,string, int, int, int, int); // Constructor
+	    void crear();
+		void leer();
+		void actualizar();
+		void borrar();
+};
+=======
+// Ejemplo Clases en C++ con (setter and getter)
+
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<fstream>
+using namespace std;
+
+
+
+
+//clase persona
+class Persona{
+	//ofstream archivo_salida("file.txt");
+	private:   //Atributos
+	    string nombres;
+	    string apellidos;
+	    string direccion;
+	    int telefono;
+    	int dia, mes, anio;
+	    
+	    //aca tengo que colocar los nuevos datos
+	public: // metodos//setters
+	    Persona(string, string,string, int, int, int, int); // Constructor
+	    void crear();
+		void leer();
+		void actualizar();
+		void borrar();
+};
+>>>>>>> 598b883d8a536164d81f2024ad5d19691a8f5091
